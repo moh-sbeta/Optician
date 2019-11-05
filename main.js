a9dd33d29055cb65391604e32b7bb2a9e2c74cbd
mohamed sbeta <mohamedsbeta@gmail.com>
2019-11-05 10:09:50 +0100

    4 commit

--- main.js
+++ main.js
@@ -22,6 +22,108 @@ var clients = [
 	},
 	dateRx: "xxx" }
 ]
+var random = function random(type){
+	return function(max){
+
+		switch (type) {
+			case "percentage":
+				return Math.floor(Math.random() * max) +" %" // to get percentage insite our data 
+			case " digit":
+				return Math.floor(Math.random() * max) +" %" // to get percentage insite our data 
+	
+			default:
+					return Math.floor(Math.random() * max) // basic case number 
+	
+		}	
+	}	
+}
+var randomUserFirstName = function (){
+	var firstNameTab = [ "Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter"]
+	return firstNameTab[random()(20)] 
+}
+var randomUserLastName = function(){
+	var lastNameTab = ["Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig"]
+	return lastNameTab[random()(20)] 	
+}
+var randomEmail = function(){
+	var provider = ["@gmail","@yahoo","@hotmail","@icloud"]
+	var domaine = [".com",".fr",".net",".org"]
+	var separator = ["-","_","."]
+	return randomUserFirstName() + separator[random()(separator.length)] + randomUserLastName() + provider[random()(provider.length)] + domaine[random()(domaine.length)]
+
+}
+var randomDate = function (){
+	var month = random()(13)
+	var monthTab = [1,3,5,7,8,10,12] // index month
+	var years = 1919 + random()(100) // to start from min of 1919
+	if(month === 2){
+		return  random()(30) + " / " + month +" / "+ years // to generate  correct number of feb month 
+	}
+	if (monthTab.indexOf(month) !== -1){
+		return  random()(32) + " / " + month +" / "+ years // to generate max coorect number of month that hold 31 days
+	}
+	return  random()(31) + " / " + month +" / "+ years // to generate max coorect number of month that hold 30 days
+}
+
+// the data generator function retun an object that will uses our keys right and left data 
+//var attr = ["SPH","CYL","AXIS","PD","ADD"]
+
+var dataObj = function (objName,attr){
+
+	var obj ={}
+	obj[objName] = {}
+
+	for( key in attr ){ 
+		obj[objName][key] = attr[key]
+	}
+
+	return obj
+}
+var attrInfo = {
+	"first Name": randomUserFirstName() ,
+	"lastName"	:randomUserLastName() , 
+	"email"	:randomEmail() ,
+	"date"	: randomDate() 
+}
+
+
+var eyes = {
+	"SPH" : random("number")(20),
+	"CYL" : random("number")(20) ,
+	"AXIS": random("percentage")(20),
+	"PD"  :	random("number")(20),
+	"ADD" : random("number")(20)
+}
+var leftObj  = dataObj("left",eyes)
+var rightObj = dataObj("right",eyes)
+var userInfo = dataObj("info",attrInfo) // we can add user id
+
+var rowObj = { "info": userInfo["info"] , "right":rightObj["right"] ,"left": leftObj["left"] }
+
+var data = function() {
+	var result = []
+	for (var i = 0 ; i< 10 ; i++ ){
+		var attrInfo = {
+			"first Name": randomUserFirstName() ,
+			"lastName"	:randomUserLastName() , 
+			"email"	:randomEmail() ,
+			"date"	: randomDate() 
+		}
+		var eyes = {
+			"SPH" : random("number")(20),
+			"CYL" : random("number")(20) ,
+			"AXIS": random("percentage")(20),
+			"PD"  :	random("number")(20),
+			"ADD" : random("number")(20)
+		}
+		var leftObj  = dataObj("left",eyes)
+		var rightObj = dataObj("right",eyes)
+		var userInfo = dataObj("info",attrInfo)
+		var rowObj = { "info": userInfo["info"] , "right":rightObj["right"] ,"left": leftObj["left"] }
+		result.push(rowObj)
+	}
+	return result
+}
 
 $('a#create').click(function(){
 	//$('a#create').css('display','contents')
